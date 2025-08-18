9e98f72076089f6812fbc9b9483fb747f684759b
Ankit Sahani <sahaniankit2611@gmail.com>
2025-08-18 21:45:43 +0530

    feat: Add ContributorDTO and ProjectContributor classes for project management

---
+++ src/main/java/com/ideacrate/backend/project/ProjectContributor.java
@@ -0,0 +1,4 @@
+package com.ideacrate.backend.project;
+
+public class ProjectContributors {
+}
---
+++ src/main/java/com/ideacrate/backend/project/ProjectContributorRepository.java
@@ -0,0 +1,4 @@
+package com.ideacrate.backend.project;
+
+public interface ProjectContributorRepository {
+}
